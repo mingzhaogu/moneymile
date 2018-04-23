786c885a429262e4648dc1c6519a1bf6f74d1c55
wcsteve <wcsteve9@gmail.com>
2018-04-23 11:41:04 -0700

    Working display of map

--- client/components/map/map_component.jsx
+++ client/components/map/map_component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import MapStyle from 'map_style';
+
 class Map extends React.Component {
   constructor(props) {
     super(props);
@@ -8,13 +10,15 @@ class Map extends React.Component {
   }
 
   componentDidMount() {
+    debugger
     this.initializeMap();
   }
 
   initializeMap() {
     const mapOptions = {
       center: { lat: 37.773972, lng: -122.431297 },
-      zoom: 13
+      zoom: 13,
+      styles: MapStyle
     }
 
     // this.map = new google.maps.Map(this.refs.renderedMap, mapOptions)
@@ -25,12 +29,16 @@ class Map extends React.Component {
   }
 
   render() {
-    debugger
     return (
-      <div ref="renderedMap" id="map-container"></div>
+      <React.Fragment>
+        <h1>hiiasdfasdfasdfiiii</h1>
+        <div ref="renderedMap" id="map-container"></div>
+        <h1>hiiasdfasdfasdfiiii</h1>
+      </React.Fragment>
     )
   }
 
 }
+// <div ref="renderedMap" id="map-container" style={{height: 500 + 'px', width: 500 + 'px'}}></div>
 
 export default Map;
