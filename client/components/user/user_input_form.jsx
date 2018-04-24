b5ba22a64147fe311a1790a519ba63b7a7441e2a
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-04-24 12:09:22 -0700

    Resolve conflicts

--- client/components/user/user_input_form.jsx
+++ client/components/user/user_input_form.jsx
@@ -37,65 +37,42 @@ class UserInputForm extends React.Component {
     let submitted;
     let formClassName;
     if (this.state.formSubmitted) {
-<<<<<<< HEAD
       submitted = "submitted";
       formClassName = "user-submitted-form"
     } else {
       submitted = "";
       formClassName = "user-input-form"
-=======
-      formName = "-submitted";
-      formClassName = "user-submitted-form";
-    } else {
-      formClassName = "user-input-form";
->>>>>>> c390f92297f0ffdf9612419d4d6cc711499f20fe
     }
 
     return (
       <form className={formClassName}
         onSubmit={this.submitForm}>
-<<<<<<< HEAD
         <p id={submitted}
           className={`question}`}>
           WHERE CAN I GO WITH $
           <input type="number"
             id={submitted}
             className={`dollar-input`}
-=======
-        <p className={`question${formName}`}>
-          WHERE CAN I GO WITH $
-          <input type="number"
-            className={`dollar-input${formName}`}
->>>>>>> c390f92297f0ffdf9612419d4d6cc711499f20fe
             value={this.state.dollarInput}
             onChange={this.updateInput("dollarInput")}
           />
           &nbsp;FROM&nbsp;
           <input type="text"
-<<<<<<< HEAD
             id={submitted}
             className={`address-input`}
-=======
-            className={`address-input${formName}`}
->>>>>>> c390f92297f0ffdf9612419d4d6cc711499f20fe
             value={this.state.addressInput}
             onChange={this.updateInput("addressInput")}
           />
           ?
         </p>
         <input type="submit"
-<<<<<<< HEAD
           id={submitted}
           className={`submit-button`}
-=======
-          className={`submit-button${formName}`}
->>>>>>> c390f92297f0ffdf9612419d4d6cc711499f20fe
           value="ask moneymile"
         />
       </form>
     );
   }
-
 }
 
 export default UserInputForm;
