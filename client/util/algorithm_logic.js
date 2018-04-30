e34cbdf1c4d6e5383e0de6389dfc9fe63296b407
Jonathan Lau <jonathan.lau@catherinewebs.com>
2018-04-30 00:42:27 -0700

    Mobile fix: change http to https call in landOrWater

--- client/util/algorithm_logic.js
+++ client/util/algorithm_logic.js
@@ -47,7 +47,7 @@ export const recalculateBoundary = function (position, boundary, map, direction,
 }
 
 export const landOrWater = function (position, map, callback) {
-  const mapUrl = `http://maps.googleapis.com/maps/api/staticmap?center=${position.lat()},${position.lng()}&zoom=${map.getZoom()}&size=1x1&maptype=roadmap&key=${process.env.GOOGLE_API_KEY}`;
+  const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${position.lat()},${position.lng()}&zoom=${map.getZoom()}&size=1x1&maptype=roadmap&key=${process.env.GOOGLE_API_KEY}`;
   const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
 
