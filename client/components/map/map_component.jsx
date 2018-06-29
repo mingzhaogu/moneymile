bd350c3b03315e07ffc95ee398e07e482d3da4dd
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-06-28 17:47:23 -0700

    Error handle invalid address

--- client/components/map/map_component.jsx
+++ client/components/map/map_component.jsx
@@ -42,7 +42,7 @@ class Map extends React.Component {
   }
 
   loadingMount() {
-    this.setState({loading: true});
+    this.setState({ loading: true });
   }
 
   initializeMap() {
@@ -150,7 +150,7 @@ class Map extends React.Component {
       this.setState({ status: "SORRY, COULDN'T FIND YOU..." });
       setTimeout(
         function() {
-          this.setState({ userAddress: '' });
+          this.setState({ userAddress: ' ' });
         }.bind(this),
         3000
       );
