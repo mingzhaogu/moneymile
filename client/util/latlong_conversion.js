f06ff2925195b7ec8399f52c86e634730b843ed3
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-06-28 17:46:40 -0700

    Incomplete revamp of ParseAdd->LtLg

--- client/util/latlong_conversion.js
+++ client/util/latlong_conversion.js
@@ -1,4 +1,28 @@
-export const parseAddressToLatLng = function(address, callback) {
+// export const parseAddressToLatLng = function(address, callback) {
+//   const geocoder = new google.maps.Geocoder;
+//   geocoder.geocode({ address: address }, (results, status) => {
+//     if (status === 'OK') {
+//       const addressLatLng = new google.maps.LatLng(
+//         results[0].geometry.location.lat(),
+//         results[0].geometry.location.lng()
+//       );
+//       this.props.centerMap(addressLatLng);
+//       this.setState({ addressLatLng }, () => {
+//         this.getBoundaries();
+//       });
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//       this.setState({
+//         addressLatLng: '',
+//         formSubmitted: false
+//       }, () => {
+//         this.getBoundaries();
+//       });
+//     }
+//   });
+// };
+
+export const parseAddressToLatLng = function(address) {
   const geocoder = new google.maps.Geocoder;
   geocoder.geocode({ address: address }, (results, status) => {
     if (status === 'OK') {
@@ -6,12 +30,10 @@ export const parseAddressToLatLng = function(address, callback) {
         results[0].geometry.location.lat(),
         results[0].geometry.location.lng()
       );
-      this.props.centerMap(addressLatLng);
-      this.setState({ addressLatLng }, () => {
-        this.getBoundaries();
-      });
+
+      return addressLatLng;
     } else {
-      alert('Geocode was not successful for the following reason: ' + status);
+      return false;
     }
   });
 };
